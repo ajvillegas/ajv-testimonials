3e7d5b9513a490940d02ff8817b1863af924edcf
Alexis J. Villegas <alexis@ajvillegas.com>
2017-05-09 16:07:52 -0700

    Update PHP comments

--- admin/class-ajv-testimonials-cpt.php
+++ admin/class-ajv-testimonials-cpt.php
@@ -44,8 +44,8 @@ class AJV_Testimonials_CPT {
 	 * Initialize the class and set its properties.
 	 *
 	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of this plugin.
-	 * @param      string    $version    The version of this plugin.
+	 * @param    string    $plugin_name		The name of this plugin.
+	 * @param    string    $version			The version of this plugin.
 	 */
 	public function __construct( $plugin_name, $version ) {
 
